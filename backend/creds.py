823d8926a8ab609d327f9568f22efd86d664ae1c
Shrneha <shraddha.bhuran31@gmail.com>
2023-05-04 17:14:38 +0530

    added fetch issues api

--- backend/creds.py
+++ backend/creds.py
@@ -1,2 +1,3 @@
-
 jira_API_token ="ATATT3xFfGF0e3mor4HG_JLxYJ6zhIUzmu4RBF3ni90ueaWQGbFDzwVCJuRBQCJknC6ktI7k0niMgRC9jEsNRJHdv3RrdxdPPS7XQ6MH5JeYQjz67tyLtdphpJeNZzHAciiEzugefULbXP1uTlJ5RE4CW2Jv49rNfZISaRJIo4r75DG3HLbdnj0=C0D9F7D9"
+jira_project_board_url="https://shrneha.atlassian.net/rest/api/3/search"
+username="shraddha.bhuran31@gmail.com"
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,52 @@
+import requests
+import json
+from creds import *
+# jira_API_token, jira_project_board_url
+from flask import Flask, jsonify, request, json
+from flask_cors import CORS
+from requests.auth import HTTPBasicAuth
 
-from creds import jira_API_token
 
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def helloworld():
+    return "Hello World !"
+
+
+# API to generate reports
+@app.route('/fetch_issues', methods=['POST', 'GET'])
+def consumption_reports():
+    """Shows basic usage of the Sheets API.
+    Print values from a sample spreadsheet.
+    """
+
+
+    headers={
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    query = {
+        'jql': 'project=TTS'
+    }
+    auth = HTTPBasicAuth(username, jira_API_token)
+
+    response=requests.request(
+        "GET",
+        jira_project_board_url,
+        headers=headers,
+        auth=auth
+    )
+    data = json.loads(response.text)
+    print(type(data))
+    return(data["issues"][0]["fields"]["summary"])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
 
 
