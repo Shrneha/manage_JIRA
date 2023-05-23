eb602ac4a80aae0f8a1f22f83637b43c3e73e65d
Shrneha <shraddha.bhuran31@gmail.com>
2023-05-23 12:23:09 +0530

    bugfixes

--- frontend/src/Components/Tickets.js
+++ frontend/src/Components/Tickets.js
@@ -1,34 +1,18 @@
 import React, {useState, useEffect} from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
     Container,
-    Table,
-    Card,
+    Table,  
     CardImg,
     CardText,
     CardTitle,
     CardBody,
-    Row, Col
+    Row, Col,
+    Button
 } from "reactstrap";
 import Axios from "axios";
-//import ViewModal from "./ViewModal";
-import { Form } from "react-bootstrap";
-// import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal';
-import styled, { css } from 'styled-components';
+import Card from 'react-bootstrap/Card';
 
-
-const Button = styled.button`
-    min-width: 100px;
-    padding: 16px 32px;
-    border-radius: 4px;
-    border: none;
-    background: #141414;
-    color: #fff;
-    cursor:pointer;
-    font-size: 24px;
-`;
 
 const FetchAPIUrl = 'http://127.0.0.1:5000/fetch_issues'
 
@@ -75,19 +59,16 @@ function Tickets() {
         fetchTickets()
     }, [])
 
-   
-
-    
+       
 
     return (
         <Container>
             <Row> 
             <Col style={{margin: '10px'}}>
-            <h4>To Do</h4> 
-                   
+            <h4>To Do</h4>                  
                 {todoData.filter(record =>record.status === "To Do").map((issue,id) => (
                 <Row key={issue.number}>    
-                    <Card>
+                    <Card style={{ width: '18rem' , color:'black'}}>
                         <CardBody>
                             <CardTitle>{issue.number}</CardTitle>
                             <Button 
@@ -136,13 +117,8 @@ function Tickets() {
                 </Row>
             ))}
             </Col>
-            </Row>
-            <Container>
-            <Button>click here
-            </Button>
             
-            </Container>
-           
+            </Row>
           
         </Container>
     )
@@ -151,3 +127,40 @@ function Tickets() {
 
 
 export default Tickets;
+
+// <Col style={{margin: '10px'}}>
+//             <h4>Done</h4>          
+//                 {todoData.filter(record =>record.status === "Done").map((issue,id) => (
+//                 <Row key={issue.number}>    
+//                     <Card>
+//                         <CardBody>
+//                             <CardTitle>{issue.number}</CardTitle>
+//                             <Button 
+//                                 color="success"
+//                                 key={issue.id}
+//                                 >{issue.status}
+//                             </Button>
+//                         </CardBody>
+//                     </Card>
+//                 </Row>
+//             ))}
+//             </Col>
+//             <Col style={{margin: '10px'}}>
+//             <h4>In Progress</h4>          
+//                 {todoData.filter(record =>record.status === "In Progress").map((issue,id) => (
+//                 <Row key={issue.number}>    
+//                     <Card>
+//                         <CardBody>
+//                             <CardTitle>{issue.number}</CardTitle>
+//                                 <Button 
+//                                     className="btn-modal"
+//                                     color="success"
+//                                     key={issue.id}
+//                                     onClick={toggleModal}
+//                                     >{issue.status}  
+//                                 </Button>
+//                         </CardBody>
+//                     </Card>
+//                 </Row>
+//             ))}
+//             </Col>
