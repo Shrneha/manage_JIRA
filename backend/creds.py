1c39be74a22955c63eea467a1c6d17826c7349bc
Shraddha bhuran <shraddha.bhuran31@gmail.com>
2023-05-31 11:53:06 +0530

    Update creds.py

--- backend/creds.py
+++ backend/creds.py
@@ -1,11 +1,11 @@
-jira_API_token ="ATATT3xFfGF0e3mor4HG_JLxYJ6zhIUzmu4RBF3ni90ueaWQGbFDzwVCJuRBQCJknC6ktI7k0niMgRC9jEsNRJHdv3RrdxdPPS7XQ6MH5JeYQjz67tyLtdphpJeNZzHAciiEzugefULbXP1uTlJ5RE4CW2Jv49rNfZISaRJIo4r75DG3HLbdnj0=C0D9F7D9"
-jira_project_board_url="https://shrneha.atlassian.net/rest/api/3/search"
-username="shraddha.bhuran31@gmail.com"
+jira_API_token = ##########################
+jira_project_board_url=############################
+username=######################
 
 # Mongodb
-DB_CONNECTION_URL = "mongodb+srv://shraddhabhuran31:w4EgJHtVCTp8nyed@jiradata.zqvrr78.mongodb.net/?retryWrites=true&w=majority"
-cluster_username ="shraddhabhuran31"
-pwd = "w4EgJHtVCTp8nyed"
+DB_CONNECTION_URL = #####################
+cluster_username =#####################
+pwd = #####################
 
 # JIRA status Update URL
 STATUS_UPDT_URL = "https://shrneha.atlassian.net/rest/api/3/issue/TICKET_NUMBER/transitions"
